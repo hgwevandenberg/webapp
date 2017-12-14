e2421851af33d37ff1ca19b3dce1dcd8d1a63c69
Grant Blakeman <hello@grantblakeman.com>
2017-12-14 11:09:22 -0800

    artist invites - fix line length

--- src/components/artist_invites/ArtistInviteRenderables.js
+++ src/components/artist_invites/ArtistInviteRenderables.js
@@ -467,7 +467,12 @@ export const ArtistInviteDetail = ({
         </div>
       </div>
     </article>
-    <ArtistInviteSubmissionsContainer links={links} slug={slug} status={status} isLoggedIn={isLoggedIn} />
+    <ArtistInviteSubmissionsContainer
+      links={links}
+      slug={slug}
+      status={status}
+      isLoggedIn={isLoggedIn}
+    />
   </div>
 )
 ArtistInviteDetail.propTypes = {
