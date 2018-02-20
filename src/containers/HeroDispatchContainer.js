041d63c00f7f31cf26a16298bf41c1186c129ee3
Alan Peabody <gapeabody@gmail.com>
2018-02-20 16:16:11 -0500

    Add profile hero support back.

--- src/containers/HeroDispatchContainer.js
+++ src/containers/HeroDispatchContainer.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import HeroPageHeaderContainer from './HeroPageHeaderContainer'
+import HeroProfileContainer from './HeroProfileContainer'
 import { loadPageHeaders } from '../actions/page_headers'
 import {
   selectPageHeaders,
   selectPageHeaderSlug,
   selectPageHeaderKind,
 } from '../selectors/page_headers'
+import { selectViewNameFromRoute } from '../selectors/routing'
 
-function mapStateToProps(state) {
+function mapStateToProps(state, props) {
   return {
+    isUserProfile: selectViewNameFromRoute(state, props) === 'userDetail',
     pageHeaderKind: selectPageHeaderKind(state),
     pageHeaderSlug: selectPageHeaderSlug(state),
     pageHeaders: selectPageHeaders(state),
+    params: props.params,
   }
 }
 
@@ -21,8 +25,10 @@ class HeroDispatchContainer extends Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     pageHeaders: PropTypes.object.isRequired,
+    params: PropTypes.object.isRequired,
     pageHeaderKind: PropTypes.string,
     pageHeaderSlug: PropTypes.string,
+    isUserProfile: PropTypes.bool.isRequired,
   }
 
   static defaultProps = {
@@ -45,13 +51,16 @@ class HeroDispatchContainer extends Component {
   }
 
   render() {
-    const { pageHeaderKind, pageHeaders } = this.props
-    if (!pageHeaders || pageHeaders.isEmpty()) { return null }
+    const { pageHeaderKind, pageHeaders, isUserProfile, params } = this.props
+    if (!isUserProfile && (!pageHeaders || pageHeaders.isEmpty())) { return null }
     switch (pageHeaderKind) {
       case 'CATEGORY':
       case 'GENERIC':
         return <HeroPageHeaderContainer />
       default:
+        if (isUserProfile) {
+          return <HeroProfileContainer params={params} />
+        }
         return null
     }
   }
---
+++ src/containers/HeroProfileContainer.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { trackEvent } from '../actions/analytics'
+import { openModal } from '../actions/modals'
+import {
+  selectUserId,
+  selectUserPostsAdultContent,
+  selectUserCoverImage,
+  selectUserUsername,
+} from '../selectors/user'
+import { selectViewsAdultContent } from '../selectors/profile'
+import { selectDPI } from '../selectors/gui'
+import { Hero } from '../components/heros/HeroRenderables'
+import {
+  HeroShareUserButton,
+  HeroScrollToContentButton,
+} from '../components/heros/HeroParts'
+import UserContainer from './UserContainer'
+import BackgroundImage from '../components/assets/BackgroundImage'
+import ShareDialog from '../components/dialogs/ShareDialog'
+import { css, media } from '../styles/jss'
+import * as s from '../styles/jso'
+
+function mapStateToProps(state, props) {
+  const userId = selectUserId(state, props)
+  const username = selectUserUsername(state, props)
+  const dpi = selectDPI(state)
+  const coverImage = selectUserCoverImage(state, props)
+  const useGif = selectViewsAdultContent(state) ||
+    !selectUserPostsAdultContent(state, props) ||
+    false
+  return { userId, dpi, coverImage, useGif, username }
+}
+
+const profileStyle = css(
+  s.relative, s.flex, s.overflowHidden,
+  media(s.minBreak2, { height: 'calc(100vh - 80px)', minHeight: 540 }),
+)
+
+class HeroProfileContainer extends Component { //eslint-disable-line
+  static propTypes = {
+    userId: PropTypes.string,
+    username: PropTypes.string,
+    dpi: PropTypes.string.isRequired,
+    coverImage: PropTypes.object,
+    useGif: PropTypes.bool.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    userId: null,
+    username: null,
+    coverImage: null,
+  }
+
+  static childContextTypes = {
+    onClickShareProfile: PropTypes.func,
+  }
+
+  getChildContext() {
+    return {
+      onClickShareProfile: this.onClickShareProfile,
+    }
+  }
+
+  onClickShareProfile = () => {
+    const { dispatch, username } = this.props
+    const action = bindActionCreators(trackEvent, dispatch)
+    dispatch(openModal(<ShareDialog username={username} trackEvent={action} />, '', null, 'open-share-dialog-profile'))
+  }
+
+  render() {
+    const { userId, dpi, coverImage, useGif } = this.props
+    if (!userId) { return null }
+    return (
+      <Hero>
+        {[
+          <div className={profileStyle} key="HeroProfile">
+            <h1>Hello</h1>
+            <BackgroundImage
+              className="inHeroProfile hasOverlay6"
+              dpi={dpi}
+              sources={coverImage}
+              useGif={useGif}
+            />
+            <UserContainer userId={userId} type="profile" />
+            <HeroShareUserButton />
+            <HeroScrollToContentButton />
+          </div>,
+        ]}
+      </Hero>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(HeroProfileContainer)
+
