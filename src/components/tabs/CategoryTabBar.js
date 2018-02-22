127805b94da32627356888db3821a075d8460770
Alan Peabody <gapeabody@gmail.com>
2018-02-22 09:52:19 -0500

    Remove flow from category tab bar.

--- src/components/tabs/CategoryTabBar.js
+++ src/components/tabs/CategoryTabBar.js
@@ -1,45 +1,29 @@
-// @flow
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router'
 import classNames from 'classnames'
-import type { CategoryTabProps } from '../../types/flowtypes'
-
-type TabProps = {
-  isActive: boolean,
-  label: string,
-  source: string,
-  to: string,
-}
 
 const backgroundStyles = source => ({
   backgroundImage: `url("${source}")`,
 })
 
-const CategoryTab = (props: TabProps) => {
-  const { isActive, label, source, to } = props
-  return (
-    <Link
-      className={classNames('CategoryTab', { isActive })}
-      to={to}
-      style={source ? backgroundStyles(source) : null}
-    >
-      <span className="CategoryTabLabel">{label}</span>
-    </Link>
-  )
+const CategoryTab = ({ isActive, label, source, to }) => (
+  <Link
+    className={classNames('CategoryTab', { isActive })}
+    to={to}
+    style={source ? backgroundStyles(source) : null}
+  >
+    <span className="CategoryTabLabel">{label}</span>
+  </Link>
+)
+CategoryTab.propTypes = {
+  isActive: PropTypes.boolean.isRequired,
+  label: PropTypes.string.isRequired,
+  source: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 }
 
-type TabBarProps = {
-  pathname: string,
-  subscribed: boolean,
-  tabs: Array<CategoryTabProps>,
-}
-
-type GlobalTabProps = {
-  pathname: string,
-}
-
-const AllCategoryTab = (props: GlobalTabProps) => {
-  const { pathname } = props
+const AllCategoryTab = ({ pathname }) => {
   const isActive = pathname === '/discover' ||
     pathname === '/discover/trending' ||
     pathname === '/discover/recent'
@@ -52,9 +36,11 @@ const AllCategoryTab = (props: GlobalTabProps) => {
     </Link>
   )
 }
+AllCategoryTab.propTypes = {
+  pathname: PropTypes.string.isRequired,
+}
 
-const SubscribedCategoryTab = (props: GlobalTabProps) => {
-  const { pathname } = props
+const SubscribedCategoryTab = ({ pathname }) => {
   const isActive = pathname === '/discover/subscribed' ||
     pathname === '/discover/subscribed/trending' ||
     pathname === '/discover/subscribed/recent'
@@ -67,36 +53,41 @@ const SubscribedCategoryTab = (props: GlobalTabProps) => {
     </Link>
   )
 }
+SubscribedCategoryTab.propTypes = {
+  pathname: PropTypes.string.isRequired,
+}
 
-export const CategoryTabBar = (props: TabBarProps) => {
-  const { pathname, tabs, subscribed } = props
-  return (
-    <div className="CategoryTabBar">
-      <nav className="CategoryTabs">
-        <AllCategoryTab pathname={pathname} />
-        {subscribed && <SubscribedCategoryTab pathname={pathname} />}
+export const CategoryTabBar = ({ pathname, tabs, subscribed }) => (
+  <div className="CategoryTabBar">
+    <nav className="CategoryTabs">
+      <AllCategoryTab pathname={pathname} />
+      {subscribed && <SubscribedCategoryTab pathname={pathname} />}
 
-        {tabs.map(tab =>
-          (<CategoryTab
-            isActive={(tab.activePattern ? tab.activePattern.test(pathname) : tab.to === pathname)}
-            key={`CategoryTab_${tab.to}`}
-            label={tab.label}
-            source={tab.source}
-            to={tab.to}
-          />),
-        )}
-      </nav>
-      <div className="CategoryTabBarUtility">
-        <Link
-          activeClassName="isActive"
-          className="CategoryUtilTab"
-          to="/discover/all"
-        >
-          <span>See All</span>
-        </Link>
-      </div>
+      {tabs.map(tab =>
+        (<CategoryTab
+          isActive={(tab.activePattern ? tab.activePattern.test(pathname) : tab.to === pathname)}
+          key={`CategoryTab_${tab.to}`}
+          label={tab.label}
+          source={tab.source}
+          to={tab.to}
+        />),
+      )}
+    </nav>
+    <div className="CategoryTabBarUtility">
+      <Link
+        activeClassName="isActive"
+        className="CategoryUtilTab"
+        to="/discover/all"
+      >
+        <span>See All</span>
+      </Link>
     </div>
-  )
+  </div>
+)
+CategoryTabBar.propTypes = {
+  pathname: PropTypes.string.isRequired,
+  subscribed: PropTypes.boolean.isRequired,
+  tabs: PropTypes.array.isRequired,
 }
 
 export default CategoryTabBar
