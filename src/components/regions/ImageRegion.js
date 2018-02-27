87ec292ee5b76b4c2b635cf2a73d35af5044d1ec
Grant Blakeman <hello@grantblakeman.com>
2018-02-26 16:58:35 -0800

    temp, log urls

--- src/components/regions/ImageRegion.js
+++ src/components/regions/ImageRegion.js
@@ -64,9 +64,10 @@ const lightBoxImageStyle = css(
 
 // export to re-use this wonky image url parser
 export function getTempAssetId(url) {
-  if (url.includes('/ello-') && url.includes('uploads/')) {
-    return url.split('uploads/')[1].split('/ello-')[1].split('.')[0].replace(/-/g, '_')
-  }
+  console.log(url)
+  // if (url.includes('/ello-') && url.includes('uploads/')) {
+  //   return url.split('uploads/')[1].split('/ello-')[1].split('.')[0].replace(/-/g, '_')
+  // }
   return null
 }
 
