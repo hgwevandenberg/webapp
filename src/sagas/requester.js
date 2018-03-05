dbcd8d0f8e76c76577dda247c8ef26783575e768
Alan Peabody <gapeabody@gmail.com>
2018-03-05 14:03:09 -0500

    Drastically reduce webappToken requests logged out
    Current editorial page sends 15 identical requests, then further pages
    all require one or two.
    
    This reduces it to one request for the duration of the visit.

--- src/sagas/requester.js
+++ src/sagas/requester.js
@@ -18,6 +18,7 @@ export const requestTypes = [
   ACTION_TYPES.AUTHENTICATION.LOGOUT,
   ACTION_TYPES.AUTHENTICATION.REFRESH,
   ACTION_TYPES.AUTHENTICATION.USER,
+  ACTION_TYPES.AUTHENTICATION.PUBLIC,
   ACTION_TYPES.COMMENT.CREATE,
   ACTION_TYPES.COMMENT.DELETE,
   ACTION_TYPES.COMMENT.EDITABLE,
@@ -201,7 +202,8 @@ export function* performRequest(action) {
   const SUCCESS = `${type}_SUCCESS`
 
   let tokenJSON = null
-  if (action.type === ACTION_TYPES.AUTHENTICATION.REFRESH) {
+  if (action.type === ACTION_TYPES.AUTHENTICATION.REFRESH ||
+    action.type === ACTION_TYPES.AUTHENTICATION.PUBLIC) {
     // access token not needed for refreshing the existing token.
     // This shortcuts a request to get a public token.
     tokenJSON = { token: { access_token: null } }
--- src/selectors/authentication.js
+++ src/selectors/authentication.js
@@ -3,17 +3,36 @@ export const selectAccessToken = state => state.authentication.get('accessToken'
 export const selectExpirationDate = state => state.authentication.get('expirationDate')
 export const selectIsLoggedIn = state => state.authentication.get('isLoggedIn')
 export const selectRefreshToken = state => state.authentication.get('refreshToken')
+export const selectPublicToken = state => state.authentication.getIn(['publicToken', 'accessToken'])
+export const selectPublicTokenExpirationDate = state =>
+  state.authentication.getIn(['publicToken', 'expirationDate'])
 
-// Memoized selectors
-export const selectShouldUseAccessToken = (state) => {
+// Autentication selectors are not memoized because of the date time check
+export const selectUnexpiredAccessToken = (state) => {
   const accessToken = selectAccessToken(state)
   const expDate = selectExpirationDate(state)
-  return accessToken && expDate > new Date()
+  if (accessToken && expDate > new Date()) {
+    return accessToken
+  }
+  return null
 }
 
-export const selectShouldUseRefreshToken = (state) => {
+export const selectValidRefreshToken = (state) => {
   const accessToken = selectAccessToken(state)
   const expDate = selectExpirationDate(state)
   const isLoggedIn = selectIsLoggedIn(state)
-  return isLoggedIn && accessToken && !(expDate > new Date())
+  const refreshToken = selectRefreshToken(state)
+  if (isLoggedIn && accessToken && !(expDate > new Date())) {
+    return refreshToken
+  }
+  return null
+}
+
+export const selectUnexpiredPublicToken = (state) => {
+  const publicToken = selectPublicToken(state)
+  const expDate = selectPublicTokenExpirationDate(state)
+  if (publicToken && expDate > new Date()) {
+    return publicToken
+  }
+  return null
 }
