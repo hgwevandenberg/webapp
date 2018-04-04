5ef331cbbbe4fc2f2fac4d894c2bd3df76e0ff26
Alan Peabody <gapeabody@gmail.com>
2018-04-04 15:35:40 -0400

    Functional search to select categories.

--- src/components/editor/CategoryPostSelector.js
+++ src/components/editor/CategoryPostSelector.js
@@ -1,9 +1,32 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 function filterSearch(categories, searchText) {
   if (searchText === '') { return categories }
   return categories.filter(c => c.get('name').toLowerCase().includes(searchText.toLowerCase()))
+}
+
+function CategoryItem({ category, index, selectedIndex, onSelect }) {
+  const isSelected = selectedIndex === index
+  return (
+    <li className={classNames({ isSelected })}>
+      <button onClick={() => onSelect(category)}>
+        {category.get('name')}
+      </button>
+    </li>
+  )
+}
+
+CategoryItem.propTypes = {
+  category: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
+  selectedIndex: PropTypes.number,
+  onSelect: PropTypes.func.isRequired,
+}
+
+CategoryItem.defaultProps = {
+  selectedIndex: null,
 }
 
 export default class CategoryPostSelector extends PureComponent {
@@ -19,7 +42,12 @@ export default class CategoryPostSelector extends PureComponent {
   constructor(props) {
     super(props)
     const { subscribedCategories, unsubscribedCategories } = props
-    this.state = { subscribedCategories, unsubscribedCategories, searchText: '' }
+    this.state = {
+      subscribedCategories,
+      unsubscribedCategories,
+      searchText: '',
+      selectedIndex: null,
+    }
   }
 
   componentDidMount() {
@@ -38,40 +66,48 @@ export default class CategoryPostSelector extends PureComponent {
     })
   }
 
-  onSearch = (event) => {
+  handleSearch = (event) => {
     const searchText = event.target.value
     const { subscribedCategories, unsubscribedCategories } = this.props
     this.setState({
+      selectedIndex: 0,
       searchText,
       subscribedCategories: filterSearch(subscribedCategories, searchText),
       unsubscribedCategories: filterSearch(unsubscribedCategories, searchText),
     })
   }
 
-  categoryItem = (category) => {
+  handleKeyDown = (event) => {
+    const { subscribedCategories, unsubscribedCategories, selectedIndex } = this.state
     const { onSelect } = this.props
-    return (
-      <li key={`categorySelect:${category.get('id')}`}>
-        <button onClick={() => onSelect(category)}>
-          {category.get('name')}
-        </button>
-      </li>
-    )
+    const categories = subscribedCategories.concat(unsubscribedCategories)
+    const max = categories.length
+    if (event.key === 'ArrowDown' && isNaN(selectedIndex)) {
+      this.setState({ selectedIndex: 0 })
+    } else if (event.key === 'ArrowDown' && selectedIndex < max) {
+      this.setState({ selectedIndex: selectedIndex + 1 })
+    } else if (event.key === 'ArrowUp' && !isNaN(selectedIndex) && selectedIndex > 0) {
+      this.setState({ selectedIndex: selectedIndex - 1 })
+    } else if (event.key === 'Enter' && !isNaN(selectedIndex)) {
+      onSelect(categories[selectedIndex])
+    }
   }
 
   render() {
-    const { selectedCategories, onClear } = this.props
-    const { subscribedCategories, unsubscribedCategories, searchText } = this.state
+    const { selectedCategories, onClear, onSelect } = this.props
+    const { subscribedCategories, unsubscribedCategories, searchText, selectedIndex } = this.state
     // Everything except this component could support multiple categories, but for now
     // we can assume there is only one selected category per post.
     const selectedCategory = selectedCategories[0]
+    const offset = subscribedCategories.length
     return (
       <div>
         {!selectedCategory &&
           <input
             type="search"
             value={searchText}
-            onChange={this.onSearch}
+            onChange={this.handleSearch}
+            onKeyDown={this.handleKeyDown}
           />
         }
         {selectedCategory &&
@@ -84,14 +120,30 @@ export default class CategoryPostSelector extends PureComponent {
           <div>
             <p>Your Categories</p>
             <ul>
-              {subscribedCategories.map(c => this.categoryItem(c))}
+              {subscribedCategories.map((category, index) =>
+                (<CategoryItem
+                  key={`categorySelect:${category.get('id')}`}
+                  category={category}
+                  index={index}
+                  selectedIndex={selectedIndex}
+                  onSelect={onSelect}
+                />),
+              )}
             </ul>
           </div>
         }
         <div>
           <p>All Categories</p>
           <ul>
-            {unsubscribedCategories.map(c => this.categoryItem(c))}
+            {unsubscribedCategories.map((category, index) =>
+              (<CategoryItem
+                key={`categorySelect:${category.get('id')}`}
+                category={category}
+                index={index + offset}
+                selectedIndex={selectedIndex}
+                onSelect={onSelect}
+              />),
+            )}
           </ul>
         </div>
       </div>
