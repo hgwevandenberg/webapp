0a5fb75b5743322a1259a3adfd848e8daf829a30
Grant Blakeman <hello@grantblakeman.com>
2018-05-09 11:50:53 -0700

    lint - remove unneeded flow disabling

--- src/components/editorials/EditorialParts.js
+++ src/components/editorials/EditorialParts.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-danger */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable javascript/flow */
 
 import React from 'react'
 import PropTypes from 'prop-types'
