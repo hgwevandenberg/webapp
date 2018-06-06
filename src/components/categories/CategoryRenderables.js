9f1e62c9e9c5ee797aeaacc2057a4afaca913c28
Grant Blakeman <hello@grantblakeman.com>
2018-06-05 18:27:50 -0700

    remove errant console

--- src/components/categories/CategoryRenderables.js
+++ src/components/categories/CategoryRenderables.js
@@ -168,7 +168,6 @@ const categorySubNavStyle = css(
 )
 
 export function CategorySubNav({ stream, kind }) {
-  console.log(`kind: ${kind}`)
   if (stream === 'global') {
     return (
       <ul className={`category-sub-nav global ${categorySubNavStyle}`}>
