548428f6b8b1a22854d2399a4902310a5b85b090
Colin T.A. Gray <colinta@gmail.com>
2018-10-02 12:39:15 -0400

    attempt a fix at notifications

--- src/components/streams/StreamFilters.js
+++ src/components/streams/StreamFilters.js
@@ -1,5 +1,9 @@
 import * as MAPPING_TYPES from '../../constants/mapping_types'
 
+export function notificationsFromActivities(activities) {
+  return { type: MAPPING_TYPES.NOTIFICATIONS, ids: activities }
+}
+
 export function settingsToggles(settings) {
   return { type: MAPPING_TYPES.SETTINGS, ids: settings }
 }
