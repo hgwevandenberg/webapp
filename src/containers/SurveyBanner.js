e77e333f97a76c17637292740a4470ed7399ba71
Colin T.A. Gray <colinta@gmail.com>
2020-02-10 16:39:07 -0500

    update survey text

--- src/containers/SurveyBanner.js
+++ src/containers/SurveyBanner.js
@@ -38,6 +38,9 @@ class SurveyBanner extends PureComponent {
   render() {
     const { isMobile } = this.props
     return (<div className={isMobile ? 'SurveyBanner' : 'SurveyBanner'}>
+      <h2>Take our survey!</h2>
+      <div>Can you take 3 minutes to answer a quick survey about your experience on Ello?</div>
+      <div>You will be opted in to a drawing for a $50 gift certificate</div>
       <button><Link to="https://tlnt.at/2UInPAq" target="_blank" >OK</Link></button>
       <button onClick={this.onClickNotNow} >Not right Now</button>
       <button onClick={this.onClickNotAgain} >Don’t show this again</button>
